a8c37f8a52b031f98e1842b8b61c5d70e9f5bd2c
khushboo <khushboo@sapnagroup.com>
2021-06-14 10:28:09 +0530

    updated password

--- dbSql/users.sql
+++ dbSql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2021 at 06:14 AM
+-- Generation Time: Jun 14, 2021 at 06:53 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
@@ -43,12 +43,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `password`, `email`, `address`, `role`, `created_date`, `status`) VALUES
-(1, 'Khushboo Raikar', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'khushboo@gmail.com', 'sio;lim,goa', 1, '2021-06-14 07:24:12', 1),
+(1, 'Khushboo Raikar', 'c3fbb4a659b7fa0d59344ba47523a43936890c5f2c6e9ca6cff95682fc1ab93f', 'khushboo@gmail.com', 'sio;lim,goa', 1, '2021-06-14 07:24:12', 1),
 (2, 'Diksha Verlekar', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'diksha@gmail.com', 'panjim,goa', 2, '2021-06-14 07:25:13', 1),
 (4, 'sneha', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'sneha@gmail.com', 'Goa', 1, '2021-06-14 08:00:24', 1),
 (5, 'sneha', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'sneha1@gmail.com', 'Goa', 1, '2021-06-14 08:02:16', 1),
 (6, 'vinda', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'vinda@gmail.com', 'Goa', 2, '2021-06-14 08:03:02', 1),
-(7, 'rahul', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'rahul@gmail.com', 'Goa', 2, '2021-06-14 08:06:14', 2);
+(7, 'rahul', 'c3fbb4a659b7fa0d59344ba47523a43936890c5f2c6e9ca6cff95682fc1ab93f', 'rahul@gmail.com', 'Goa', 2, '2021-06-14 08:06:14', 2);
 
 --
 -- Indexes for dumped tables
